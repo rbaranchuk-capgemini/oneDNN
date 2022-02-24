d55288f59d3be861aca5bf7a9fe066651292f5e0
chenhuwa <chenhu.wang@intel.com>
2022-02-24 23:46:47 +0800

    equality_uni_xxx_for_sse_and_avx

--- src/cpu/x64/jit_generator.hpp
+++ src/cpu/x64/jit_generator.hpp
@@ -628,7 +628,7 @@ public:
         if (is_valid_isa(avx))
             vaddps(x, op1, op2);
         else {
-            assert(x.getIdx() == op1.getIdx());
+            if (!x.isEqualIfNotInherited(op1)) movups(x, op1);
             addps(x, op2);
         }
     }
@@ -732,7 +732,7 @@ public:
         if (is_valid_isa(avx))
             vsubps(x, op1, op2);
         else {
-            assert(x.isEqualIfNotInherited(op1));
+            if (!x.isEqualIfNotInherited(op1)) movups(x, op1);
             subps(x, op2);
         }
     }
